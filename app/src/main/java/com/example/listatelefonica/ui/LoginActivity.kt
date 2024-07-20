ca8aeaffb5981fe4ef7712540922eab24b4c6c81
Artur Oliveira <ao.ragazzi@gmail.com>
2024-07-20 02:15:49 -0300

    Criando toolba e menus, Criando metodo logout

--- app/src/main/java/com/example/listatelefonica/ui/LoginActivity.kt
+++ app/src/main/java/com/example/listatelefonica/ui/LoginActivity.kt
@@ -1,6 +1,9 @@
 package com.example.listatelefonica.ui
 
+
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -15,10 +18,22 @@ class LoginActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityLoginBinding.inflate(layoutInflater)
     }
+    private lateinit var sharedPreferences: SharedPreferences
 
 
     private val userService by lazy {
         UserService(this)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        sharedPreferences = application.getSharedPreferences("login", Context.MODE_PRIVATE)
+
+        val savedUsername = sharedPreferences.getString("username", null)
+        if (savedUsername != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
     }
 
 
@@ -32,17 +47,32 @@ class LoginActivity : AppCompatActivity() {
             insets
         }
 
+
+
         binding.txtSignup.setOnClickListener {
+
             startActivity(Intent(this, SignUpActivity::class.java))
         }
 
 
         binding.btnLogin.setOnClickListener {
-            if(userService.login(binding.editUsername.text.toString(),binding.editPassword.text.toString())){
+            if (userService.login(binding.editUsername.text.toString(),binding.editPassword.text.toString())) {
+                //Nesse ponto o usuario ja foi validado, entao não e necessario realizar o login no onStart
+                if (binding.cbKeepLogin.isChecked) {
+                    keepLogin()
+                }
                 startActivity(Intent(this, MainActivity::class.java))
-            }else{
+                finish()
+
+            } else {
                 Toast.makeText(this, "Username or password incorrect", Toast.LENGTH_LONG).show()
             }
         }
+    }
+
+    fun keepLogin() {
+        val editor: SharedPreferences.Editor = sharedPreferences.edit()
+        editor.putString("username", binding.editUsername.text.toString())
+        editor.apply()
     }
 }
--- app/src/main/java/com/example/listatelefonica/ui/MainActivity.kt
+++ app/src/main/java/com/example/listatelefonica/ui/MainActivity.kt
@@ -1,21 +1,82 @@
 package com.example.listatelefonica.ui
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuProvider
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.listatelefonica.R
+import com.example.listatelefonica.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //Ativa a Toolbar
+        setSupportActionBar(binding.tbContact)
+
+    }
+
+
+    //Menu
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.search->{
+                return true
+            }
+            R.id.profile->{
+                return true
+            }
+            R.id.logout->{
+                logout()
+                return true
+            }
+            else-> super.onOptionsItemSelected(item)
+        }
+
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu,menu)
+        return true
+    }
+
+
+     fun logout() {
+        AlertDialog.Builder(this)
+            .setTitle("Logout")
+            .setMessage("Do you really want to Log out?")
+            .setNegativeButton("No"){dialog, posicao ->}
+            .setPositiveButton("Yes"){dialog, posicao->
+                val sharedPreferences = application.getSharedPreferences("login", Context.MODE_PRIVATE)
+                val editor: SharedPreferences.Editor = sharedPreferences.edit()
+                editor.putString("username",null)
+                editor.apply()
+                startActivity(Intent(this, LoginActivity::class.java))
+                finish()
+
+            }
+            .create().show()
     }
 }
